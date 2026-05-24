15425745c07f97721c9ab3f34d35b9fac5f98c7d
MaksFlyOk <maksflyok@gmail.com>
2026-05-24 16:58:58 +0700

    Update comments controller & resolver

--- src/articles/core/comments/comments.controller.ts
+++ src/articles/core/comments/comments.controller.ts
@@ -1,95 +1,157 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentEntity } from 'src/articles/data/entities/comment/comment.entity';
-import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { CreateCommentInput } from 'src/articles/dto/comment/create-comment.input';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { OptionalJwtAuthGuard } from 'src/auth/guards/optional-jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { toCommentResponse } from 'src/articles/dto/response/mappers';
 
 @Controller('comments')
 export class CommentsController {
-    constructor(private readonly commentsService: CommentsService) { }
+  constructor(private readonly commentsService: CommentsService) {}
 
-    @Post()
-    @ApiOperation({ summary: 'Создать новый комментарий' })
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                username: { type: 'string', example: 'IvanIvanov' },
-                content: { type: 'string', example: 'Отличная статья!' },
-                articleId: { type: 'string', example: 'uuid-статьи' },
-            },
-            required: ['username', 'content', 'articleId'],
+  @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard) // Доступно только авторизированному пользователю
+  @ApiOperation({ summary: 'Создать новый комментарий' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', example: 'IvanIvanov' },
+        content: { type: 'string', example: 'Отличная статья!' },
+        articleId: { type: 'string', example: 'uuid-статьи' },
+      },
+      required: ['username', 'content', 'articleId'],
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Комментарий успешно создан' })
+  async create(
+    @Body() dto: CreateCommentInput,
+    @CurrentUser('id') userId: string,
+    @CurrentUser('username') username: string,
+  ) {
+    const comment = await this.commentsService.create({
+      ...dto,
+      authorId: userId,
+      username,
+    });
+
+    return toCommentResponse(comment!);
+  }
+
+  @Get('article/:articleId')
+  @ApiBearerAuth()
+  @UseGuards(OptionalJwtAuthGuard) // Нужно для получения в теле ответа оценки авторизированного пользователя
+  @ApiOperation({ summary: 'Получить все комментарии к статье' })
+  @ApiParam({
+    name: 'articleId',
+    description: 'ID статьи, комментарии которой нужно найти',
+  })
+  @ApiResponse({ status: 200, description: 'Список комментариев получен' })
+  async getByArticle(
+    @Param('articleId') articleId: string,
+    @CurrentUser('id') userId?: string,
+  ) {
+    const comments = await this.commentsService.findByArticle(
+      articleId,
+      userId,
+    );
+
+    return comments.map(toCommentResponse);
+  }
+
+  @Patch(':id/rating')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard) // Доступно только авторизированному пользователю
+  @ApiOperation({ summary: 'Изменить рейтинг комментария' })
+  @ApiParam({ name: 'id', description: 'ID комментария' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        rating: {
+          type: 'number',
+          example: 5,
+          description: 'Новое значение рейтинга',
         },
-    })
-    @ApiResponse({ status: 201, description: 'Комментарий успешно создан' })
-    create(@Body() dto: CreateCommentInput) {
-        return this.commentsService.create(dto);
-    }
+      },
+      required: ['rating'],
+    },
+  })
+  @ApiResponse({ status: 200, description: 'Рейтинг успешно обновлен' })
+  async changeRating(
+    @Param('id') id: string,
+    @Body('rating') rating: number,
+    @CurrentUser('id') userId: string,
+  ) {
+    const comment = await this.commentsService.updateRating(id, rating, userId);
 
-    @Get('article/:articleId')
-    @ApiOperation({ summary: 'Получить все комментарии к статье' })
-    @ApiParam({
-        name: 'articleId',
-        description: 'ID статьи, комментарии которой нужно найти',
-    })
-    @ApiResponse({ status: 200, description: 'Список комментариев получен' })
-    getByArticle(@Param('articleId') articleId: string) {
-        return this.commentsService.findByArticle(articleId);
-    }
+    return toCommentResponse(comment);
+  }
 
-    @Patch(':id/rating')
-    @ApiOperation({ summary: 'Изменить рейтинг комментария' })
-    @ApiParam({ name: 'id', description: 'ID комментария' })
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                rating: { type: 'number', example: 5, description: 'Новое значение рейтинга' },
-            },
-            required: ['rating'],
-        },
-    })
-    @ApiResponse({ status: 200, description: 'Рейтинг успешно обновлен' })
-    changeRating(
-        @Param('id') id: string,
-        @Body('rating') rating: number,
-    ) {
-        return this.commentsService.updateRating(id, rating);
-    }
+  @Post('up-rating/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard) // Доступно только авторизированному пользователю
+  @ApiOperation({ summary: 'Увеличить рейтинг комментария на 1' })
+  @ApiParam({ name: 'id', description: 'ID комментария' })
+  @ApiResponse({ status: 200, description: 'Рейтинг успешно увеличен' })
+  async upRating(@Param('id') id: string, @CurrentUser('id') userId: string) {
+    const comment = await this.commentsService.upRating(id, userId);
 
-    @Post('up-rating/:id')
-    @ApiOperation({ summary: 'Увеличить рейтинг комментария на 1' })
-    @ApiParam({ name: 'id', description: 'ID комментария' })
-    @ApiResponse({ status: 200, description: 'Рейтинг успешно увеличен' })
-    upRating(@Param('id') id: string) {
-        return this.commentsService.upRating(id);
-    }
+    return toCommentResponse(comment);
+  }
 
-    @Post('down-rating/:id')
-    @ApiOperation({ summary: 'Уменьшить рейтинг комментария на 1' })
-    @ApiParam({ name: 'id', description: 'ID комментария' })
-    @ApiResponse({ status: 200, description: 'Рейтинг успешно уменьшен' })
-    downRating(@Param('id') id: string) {
-        return this.commentsService.downRating(id);
-    }
+  @Post('down-rating/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard) // Доступно только авторизированному пользователю
+  @ApiOperation({ summary: 'Уменьшить рейтинг комментария на 1' })
+  @ApiParam({ name: 'id', description: 'ID комментария' })
+  @ApiResponse({ status: 200, description: 'Рейтинг успешно уменьшен' })
+  async downRating(@Param('id') id: string, @CurrentUser('id') userId: string) {
+    const comment = await this.commentsService.downRating(id, userId);
 
-    @Post('vote/:id')
-    @ApiOperation({ summary: 'Поставить оценку комментария (1-5)' })
-    @ApiParam({ name: 'id', description: 'ID комментария' })
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                vote: { type: 'number', example: 5, description: 'Оценка от 1 до 5' },
-            },
-            required: ['vote'],
-        },
-    })
-    @ApiResponse({ status: 200, description: 'Голос успешно добавлен' })
-    vote(
-        @Param('id') commentId: string,
-        @Body('vote') vote: number,
-    ) {
-        return this.commentsService.addVote(commentId, vote);
-    }
+    return toCommentResponse(comment);
+  }
+
+  @Post('vote/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard) // Доступно только авторизированному пользователю
+  @ApiOperation({ summary: 'Поставить оценку комментария (1-5)' })
+  @ApiParam({ name: 'id', description: 'ID комментария' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        vote: { type: 'number', example: 5, description: 'Оценка от 1 до 5' },
+      },
+      required: ['vote'],
+    },
+  })
+  @ApiResponse({ status: 200, description: 'Голос успешно добавлен' })
+  async vote(
+    @Param('id') commentId: string,
+    @Body('vote') vote: number,
+    @CurrentUser('id') userId: string,
+  ) {
+    const comment = await this.commentsService.addVote(commentId, vote, userId);
+
+    return toCommentResponse(comment);
+  }
 }
--- src/articles/core/comments/comments.resolver.ts
+++ src/articles/core/comments/comments.resolver.ts
@@ -1,50 +1,83 @@
-import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID, Int } from '@nestjs/graphql';
 import { CommentsService } from './comments.service';
 import { CommentEntity } from 'src/articles/data/entities/comment/comment.entity';
 import { CreateCommentInput } from 'src/articles/dto/comment/create-comment.input';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { OptionalGqlAuthGuard } from 'src/auth/guards/optional-jwt-auth.guard';
+import { GqlAuthGuard } from 'src/auth/guards/graphql-auth.guard';
 
 @Resolver(() => CommentEntity)
 export class CommentsResolver {
-    constructor(private readonly commentsService: CommentsService) { }
+  constructor(private readonly commentsService: CommentsService) {}
 
-    @Query(() => CommentEntity, { name: 'comment', nullable: true })
-    async getComment(@Args('id', { type: () => ID }) id: string) {
-        return this.commentsService.findOne(id);
-    }
+  @Query(() => CommentEntity, { name: 'comment', nullable: true })
+  @UseGuards(OptionalGqlAuthGuard) // Нужно для получения в теле ответа оценки авторизированного пользователя
+  async getComment(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser('id') userId?: string,
+  ) {
+    return this.commentsService.findOne(id, userId);
+  }
 
-    @Query(() => [CommentEntity], { name: 'commentsByArticle' })
-    async getCommentsByArticle(@Args('articleId', { type: () => ID }) articleId: string) {
-        return this.commentsService.findByArticle(articleId);
-    }
+  @Query(() => [CommentEntity], { name: 'commentsByArticle' })
+  @UseGuards(OptionalGqlAuthGuard) // Нужно для получения в теле ответа оценки авторизированного пользователя
+  async getCommentsByArticle(
+    @Args('articleId', { type: () => ID }) articleId: string,
+    @CurrentUser('id') userId?: string,
+  ) {
+    return this.commentsService.findByArticle(articleId, userId);
+  }
 
-    @Mutation(() => CommentEntity)
-    async createComment(@Args('createComment') createComment: CreateCommentInput) {
-        return this.commentsService.create(createComment);
-    }
+  @Mutation(() => CommentEntity)
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  async createComment(
+    @Args('createComment') createComment: CreateCommentInput,
+    @CurrentUser('id') userId: string,
+    @CurrentUser('username') username: string,
+  ) {
+    return this.commentsService.create({
+      ...createComment,
+      authorId: userId,
+      username,
+    });
+  }
 
-    @Mutation(() => CommentEntity)
-    async updateCommentRating(
-        @Args('id', { type: () => ID }) id: string,
-        @Args('rating') rating: number,
-    ) {
-        return this.commentsService.updateRating(id, rating);
-    }
+  @Mutation(() => CommentEntity)
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  async updateCommentRating(
+    @Args('id', { type: () => ID }) id: string,
+    @Args('rating') rating: number,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.commentsService.updateRating(id, rating, userId);
+  }
 
-    @Mutation(() => CommentEntity)
-    async commentRatingUp(@Args('id', { type: () => ID }) id: string) {
-        return this.commentsService.upRating(id);
-    }
+  @Mutation(() => CommentEntity)
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  async commentRatingUp(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.commentsService.upRating(id, userId);
+  }
 
-    @Mutation(() => CommentEntity)
-    async commentRatingDown(@Args('id', { type: () => ID }) id: string) {
-        return this.commentsService.downRating(id);
-    }
+  @Mutation(() => CommentEntity)
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  async commentRatingDown(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.commentsService.downRating(id, userId);
+  }
 
-    @Mutation(() => CommentEntity)
-    async voteComment(
-        @Args('id', { type: () => ID }) id: string,
-        @Args('vote') vote: number,
-    ) {
-        return this.commentsService.addVote(id, vote);
-    }
+  @Mutation(() => CommentEntity)
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  async voteComment(
+    @Args('id', { type: () => ID }) id: string,
+    @Args('vote', { type: () => Int }) vote: number,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.commentsService.addVote(id, vote, userId);
+  }
 }
