f29b8193fd87949f95d3c6c46f5d871b6311e4b3
MaksFlyOk <maksflyok@gmail.com>
2026-05-24 16:54:25 +0700

    Update from prettier

--- src/articles/dto/article/articles-query.input.ts
+++ src/articles/dto/article/articles-query.input.ts
@@ -2,12 +2,12 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class ArticlesQueryInput {
-    @Field(() => Int, { defaultValue: 1 })
-    page: number = 1;
+  @Field(() => Int, { defaultValue: 1 })
+  page: number = 1;
 
-    @Field(() => Int, { defaultValue: 10 })
-    limit: number = 10;
+  @Field(() => Int, { defaultValue: 10 })
+  limit: number = 10;
 
-    @Field(() => Boolean, { defaultValue: false })
-    cumulative: boolean = false;
+  @Field(() => Boolean, { defaultValue: false })
+  cumulative: boolean = false;
 }
