dfc18557eee1c3ba64bee8722aa092ce7c879e5d
MaksFlyOk <maksflyok@gmail.com>
2026-05-24 16:57:01 +0700

    Update articles controller & resolver

--- src/articles/core/articles/articles.resolver.ts
+++ src/articles/core/articles/articles.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, ID, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { ArticleEntity } from 'src/articles/data/entities/article/article.entity';
 import { ArticlesQueryInput } from 'src/articles/dto/article/articles-query.input';
@@ -11,72 +11,90 @@ import { GqlAuthGuard } from 'src/auth/guards/graphql-auth.guard';
 import { RoleGuard } from 'src/auth/guards/role.guard';
 import { UserRole } from 'src/users/enums/roles';
 import { ArticlesService } from './articles.service';
+import { OptionalGqlAuthGuard } from 'src/auth/guards/optional-jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Resolver(() => ArticleEntity)
 export class ArticlesResolver {
-    constructor(private readonly articlesService: ArticlesService) { }
+  constructor(private readonly articlesService: ArticlesService) {}
 
-    @Query(() => ArticleEntity, { name: 'article', nullable: true })
-    async getOne(@Args('id', { type: () => ID }) id: string) {
-        return this.articlesService.findOne(id);
-    }
+  @Query(() => ArticleEntity, { name: 'article', nullable: true })
+  @UseGuards(OptionalGqlAuthGuard) // Нужно для получения в теле ответа оценки авторизированного пользователя
+  async getOne(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser('id') userId?: string,
+  ) {
+    return this.articlesService.findOne(id, userId);
+  }
 
-    @Query(() => ArticlesResponseOutput, { name: 'articles' })
-    async getAll(
-        @Args('query', { type: () => ArticlesQueryInput, nullable: true }) query?: ArticlesQueryInput,
-    ) {
-        return this.articlesService.findAll(
-            query?.page ?? 1,
-            query?.limit ?? 10,
-            query?.cumulative ?? false,
-        );
-    }
+  @Query(() => ArticlesResponseOutput, { name: 'articles' })
+  @UseGuards(OptionalGqlAuthGuard) // Нужно для получения в теле ответа оценки авторизированного пользователя
+  async getAll(
+    @Args('query', { type: () => ArticlesQueryInput, nullable: true })
+    query?: ArticlesQueryInput,
+    @CurrentUser('id') userId?: string,
+  ) {
+    return this.articlesService.findAll(
+      query?.page ?? 1,
+      query?.limit ?? 10,
+      query?.cumulative ?? false,
+      userId,
+    );
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(GqlAuthGuard, RoleGuard)
-    @Roles(UserRole.ADMIN)
-    @Mutation(() => ArticleEntity)
-    async createArticle(
-        @Args('createArticleInput') createArticleInput: CreateArticleDto,
-    ) {
-        return this.articlesService.create(createArticleInput);
-    }
+  @ApiBearerAuth()
+  @UseGuards(GqlAuthGuard, RoleGuard)
+  @Roles(UserRole.ADMIN)
+  @Mutation(() => ArticleEntity)
+  async createArticle(
+    @Args('createArticleInput') createArticleInput: CreateArticleDto,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.articlesService.create({
+      ...createArticleInput,
+      authorId: userId,
+    });
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(GqlAuthGuard, RoleGuard)
-    @Roles(UserRole.ADMIN)
-    @Mutation(() => ArticleEntity)
-    async updateArticle(
-        @Args('id', { type: () => ID }) id: string,
-        @Args('updateArticleInput') updateArticleInput: UpdateArticleInput,
-    ) {
-        return this.articlesService.update(id, updateArticleInput);
-    }
+  @ApiBearerAuth()
+  @UseGuards(GqlAuthGuard, RoleGuard)
+  @Roles(UserRole.ADMIN)
+  @Mutation(() => ArticleEntity)
+  async updateArticle(
+    @Args('id', { type: () => ID }) id: string,
+    @Args('updateArticleInput') updateArticleInput: UpdateArticleInput,
+  ) {
+    return this.articlesService.update(id, updateArticleInput);
+  }
 
-    @Mutation(() => ArticleEntity)
-    async articleRatingUp(@Args('id', { type: () => ID }) id: string) {
-        return this.articlesService.ratingUp(id);
-    }
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  @Mutation(() => ArticleEntity)
+  async articleRatingUp(@Args('id', { type: () => ID }) id: string) {
+    return this.articlesService.ratingUp(id);
+  }
 
-    @Mutation(() => ArticleEntity)
-    async articleRatingDown(@Args('id', { type: () => ID }) id: string) {
-        return this.articlesService.ratingDown(id);
-    }
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  @Mutation(() => ArticleEntity)
+  async articleRatingDown(@Args('id', { type: () => ID }) id: string) {
+    return this.articlesService.ratingDown(id);
+  }
 
-    @ApiBearerAuth()
-    @UseGuards(GqlAuthGuard, RoleGuard)
-    @Roles(UserRole.ADMIN)
-    @Mutation(() => Boolean)
-    async removeArticle(@Args('id', { type: () => ID }) id: string) {
-        await this.articlesService.remove(id);
-        return true;
-    }
+  @ApiBearerAuth()
+  @UseGuards(GqlAuthGuard, RoleGuard)
+  @Roles(UserRole.ADMIN)
+  @Mutation(() => Boolean)
+  async removeArticle(@Args('id', { type: () => ID }) id: string) {
+    await this.articlesService.remove(id);
+    return true;
+  }
 
-    @Mutation(() => ArticleEntity)
-    async voteArticle(
-        @Args('id', { type: () => ID }) id: string,
-        @Args('vote') vote: number,
-    ) {
-        return this.articlesService.addVote(id, vote);
-    }
+  @UseGuards(GqlAuthGuard) // Доступно только авторизированному пользователю
+  @Mutation(() => ArticleEntity)
+  async voteArticle(
+    @Args('id', { type: () => ID }) id: string,
+    @Args('vote', { type: () => Int }) vote: number,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.articlesService.addVote(id, vote, userId);
+  }
 }
