4c469e37394a3150049876ae359352fb28375b40
MaksFlyOk <maksflyok@gmail.com>
2026-05-24 16:51:25 +0700

    Update category, article & comment entities

--- src/articles/data/entities/category/category.ts
+++ src/articles/data/entities/category/category.ts
@@ -1,27 +1,34 @@
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { ArticleEntity } from "../article/article.entity";
-import { Field, ID, ObjectType } from "@nestjs/graphql";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ArticleEntity } from '../article/article.entity';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 @Entity('category')
 export class CategoryEntity {
-    @Field(() => ID)
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Field()
-    @Column({ unique: true })
-    name: string;
+  @Field()
+  @Column({ unique: true })
+  name: string;
 
-    @Field()
-    @CreateDateColumn()
-    createdAt: Date;
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @Field({ nullable: true })
-    @UpdateDateColumn()
-    updatedAt: Date;
+  @Field({ nullable: true })
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @Field(() => [ArticleEntity], { nullable: 'itemsAndList' })
-    @OneToMany(() => ArticleEntity, (article) => article.category, { lazy: true })
-    articles: ArticleEntity[];
+  @Field(() => [ArticleEntity], { nullable: 'itemsAndList' })
+  @OneToMany(() => ArticleEntity, (article) => article.category, { lazy: true })
+  articles: ArticleEntity[];
 }
